28909a541ebf9a38454ca862470d2f8c898a598f
manuelhaendel <manuelhaendel@t-online.de>
2020-07-20 15:33:06 +0200

    initial commit

---
+++ day01_02_exc_answer.R
@@ -0,0 +1,11 @@
+library(ncdf4)
+library(raster)
+
+prcp <- nc_open(file.path(getwd(), "day01/material/prcp_daily_2000-2000.nc"))
+tmax <- nc_open(file.path(getwd(), "day01/material/tmax_daily_2000-2000.nc"))
+tmin <- nc_open(file.path(getwd(), "day01/material/tmin_daily_2000-2000.nc"))
+
+str(tmin)
+
+
+prcp_ras <- raster(prcp)
